95d99d52b3c07094ed8982babab5014336688ec7
Dante Herrera <dherrera1625@yahoo.com>
2022-10-13 10:18:55 -0500

    Merge overlays sucessfully, fix name changer

--- scripts/boostScript.js
+++ scripts/boostScript.js
@@ -1,6 +1,6 @@
 import { WsSubscribers } from './WS.js';
 
-let boostFill = {gradient: []}
+let boostFill = { gradient: [] }
 let orangeGradient = ['#FCC80C', '#FC0C0C'];
 let blueGradient = ['#0CF4FC', '#0C58FC'];
 let boostAmount = .33;
@@ -9,59 +9,59 @@ let boostAmount = .33;
 $.circleProgress.defaults.arcCoef = 0.5; // range: 0..1
 $.circleProgress.defaults.startAngle = 0.5 * Math.PI;
 
-$.circleProgress.defaults.drawArc = function(v) {
-    var ctx = this.ctx,
-        r = this.radius,
-        t = this.getThickness(),
-        c = this.arcCoef,
-        a = this.startAngle + (1 - c) * Math.PI;
-    
-    v = Math.max(0, Math.min(1, v));
+$.circleProgress.defaults.drawArc = function (v) {
+  var ctx = this.ctx,
+    r = this.radius,
+    t = this.getThickness(),
+    c = this.arcCoef,
+    a = this.startAngle + (1 - c) * Math.PI;
 
+  v = Math.max(0, Math.min(1, v));
+
+  ctx.save();
+  ctx.beginPath();
+
+  if (!this.reverse) {
+    ctx.arc(r, r, r - t / 2, a, a + 2 * c * Math.PI * v);
+  } else {
+    ctx.arc(r, r, r - t / 2, a + 2 * c * Math.PI, a + 2 * c * (1 - v) * Math.PI, a);
+  }
+
+  ctx.lineWidth = t;
+  ctx.lineCap = this.lineCap;
+  ctx.strokeStyle = this.arcFill;
+  ctx.stroke();
+  ctx.restore();
+};
+
+$.circleProgress.defaults.drawEmptyArc = function (v) {
+  var ctx = this.ctx,
+    r = this.radius,
+    t = this.getThickness(),
+    c = this.arcCoef,
+    a = this.startAngle + (1 - c) * Math.PI;
+
+  v = Math.max(0, Math.min(1, v));
+
+  if (v < 1) {
     ctx.save();
     ctx.beginPath();
 
-    if (!this.reverse) {
-        ctx.arc(r, r, r - t / 2, a, a + 2 * c * Math.PI * v);
+    if (v <= 0) {
+      ctx.arc(r, r, r - t / 2, a, a + 2 * c * Math.PI);
     } else {
-        ctx.arc(r, r, r - t / 2, a + 2 * c * Math.PI, a + 2 * c * (1 - v) * Math.PI, a);
+      if (!this.reverse) {
+        ctx.arc(r, r, r - t / 2, a + 2 * c * Math.PI * v, a + 2 * c * Math.PI);
+      } else {
+        ctx.arc(r, r, r - t / 2, a, a + 2 * c * (1 - v) * Math.PI);
+      }
     }
 
     ctx.lineWidth = t;
-    ctx.lineCap = this.lineCap;
-    ctx.strokeStyle = this.arcFill;
+    ctx.strokeStyle = this.emptyFill;
     ctx.stroke();
     ctx.restore();
-};
-
-$.circleProgress.defaults.drawEmptyArc = function(v) {
-    var ctx = this.ctx,
-        r = this.radius,
-        t = this.getThickness(),
-        c = this.arcCoef,
-        a = this.startAngle + (1 - c) * Math.PI;
-
-    v = Math.max(0, Math.min(1, v));
-    
-    if (v < 1) {
-        ctx.save();
-        ctx.beginPath();
-
-        if (v <= 0) {
-            ctx.arc(r, r, r - t / 2, a, a + 2 * c * Math.PI);
-        } else {
-            if (!this.reverse) {
-                ctx.arc(r, r, r - t / 2, a + 2 * c * Math.PI * v, a + 2 * c * Math.PI);
-            } else {
-                ctx.arc(r, r, r - t / 2, a, a + 2 * c * (1 - v) * Math.PI);
-            }
-        }
-
-        ctx.lineWidth = t;
-        ctx.strokeStyle = this.emptyFill;
-        ctx.stroke();
-        ctx.restore();
-    }
+  }
 };
 
 (function circle($) {
@@ -76,8 +76,8 @@ $.circleProgress.defaults.drawEmptyArc = function(v) {
     lineCap: 'butt',
     fill: boostFill,
     emptyFill: '#2c2c2c'
-  }).on('circle-animation-progress', function(event, progress, stepValue) {
-    $(this).find('strong').text((stepValue*100).toFixed(0));
+  }).on('circle-animation-progress', function (event, progress, stepValue) {
+    $(this).find('strong').text((stepValue * 100).toFixed(0));
   });
   $(() => {
     WsSubscribers.init(49322, true);
@@ -86,30 +86,30 @@ $.circleProgress.defaults.drawEmptyArc = function(v) {
       let focusPlayer = d.game.target
 
       try {
-        
-        if (d.players[focusPlayer].team == 0) {
-        $('.boost.circle').css('display', 'block');
-        boostAmount = (d.players[focusPlayer].boost)/100;
 
-        $('.boost.circle').circleProgress('value', (d.players[focusPlayer].boost)/100);
-        boostFill.gradient = blueGradient;
-        $('.boost.circle').circleProgress('fill', {gradient: blueGradient});
-      
+        if (d.players[focusPlayer].team == 0) {
+          $('.boost.circle').css('display', 'block');
+          boostAmount = (d.players[focusPlayer].boost) / 100;
+
+          $('.boost.circle').circleProgress('value', (d.players[focusPlayer].boost) / 100);
+          boostFill.gradient = blueGradient;
+          $('.boost.circle').circleProgress('fill', { gradient: blueGradient });
+
 
         } else if (d.players[focusPlayer].team == 1) {
           $('.boost.circle').css('display', 'block');
-          boostAmount = (d.players[focusPlayer].boost)/100;
+          boostAmount = (d.players[focusPlayer].boost) / 100;
 
-          $('.boost.circle').circleProgress('value', (d.players[focusPlayer].boost)/100);
+          $('.boost.circle').circleProgress('value', (d.players[focusPlayer].boost) / 100);
           boostFill.gradient = orangeGradient;
-          $('.boost.circle').circleProgress('fill', {gradient: orangeGradient});
-        } 
+          $('.boost.circle').circleProgress('fill', { gradient: orangeGradient });
+        }
 
       } catch (e) {
         $('.boost.circle').css('display', 'none');
       }
-    
-  });
-    
+
+    });
+
   });
 })(jQuery);
--- scripts/valueHandler.js
+++ scripts/valueHandler.js
@@ -1,16 +1,51 @@
-let seriesTitle = "";
-let roundTitle = "";
-let seriesLength = 1;
-let gameNumber = 1;
+import { WsSubscribers } from "./WS.js";
+
+let ws = new WebSocket('ws://localhost:6969');
+
+const secondsToTime = (seconds, ms, showMs) => {
+    if (seconds == 0) {
+        return "00.0";
+    } if (showMs) {
+        if (ms % 1 == 0) { ms = ms - 1 }
+        console.log((new Date(ms * 1000).toISOString().substr(showMs ? 17 : 15, 4)));
+        return (new Date(ms * 1000).toISOString().substr(showMs ? 17 : 15, 4));
+    }
+    return (new Date(seconds * 1000).toISOString().substr(showMs ? 17 : 15, 4));
+
+};
+
+const findPlayers = (playerGameObj, teamNum) => {
+    let playerList = [];
+    let teamNumList = [];
+    let teamList = [];
+    for (let item in playerGameObj) {
+        if (playerGameObj.hasOwnProperty(item)) {
+            let value = playerGameObj[item];
+            playerList.push(item);
+        }
+    }
+    for (let player in playerList) {
+        let primePlayer = playerList[player];
+        if (playerGameObj[primePlayer]['team'] == teamNum) {
+            teamNumList.push(playerGameObj[primePlayer]["name"]);
+        }
+    }
+    return teamNumList
+};
 let leftTeamName = "";
 let rightTeamName = "";
-let lTeamSScore = 1;
-let rTeamSScore = 1;
-
-const sLengthOptions = 7;
-const gNumberOptions = 7;
-
 const valueHandler = () => {
+    let seriesTitle = "";
+    let roundTitle = "";
+    let seriesLength = 1;
+    let gameNumber = 1;
+
+    let lTeamSScore = 1;
+    let rTeamSScore = 1;
+    let nameOverride = false;
+
+    const sLengthOptions = 7;
+    const gNumberOptions = 7;
     seriesTitle = document.getElementById('form-value-input1').value;
     roundTitle = document.getElementById('form-value-input2').value;
     let i = 1;
@@ -34,7 +69,11 @@ const valueHandler = () => {
     lTeamSScore = document.getElementById('form-value-input7').value;
     rTeamSScore = document.getElementById('form-value-input8').value;
 
+
+    (leftTeamName != '') ? nameOverride = true : nameOverride = false;
+
     return {
+        "event": "control-panel-info",
         seriesTitle,
         roundTitle,
         seriesLength,
@@ -42,14 +81,85 @@ const valueHandler = () => {
         leftTeamName,
         rightTeamName,
         lTeamSScore,
-        rTeamSScore
+        rTeamSScore,
+        nameOverride
     };
 }
-// console.log("seriesTitle " + seriesTitle)
-// console.log("roundTitle " + roundTitle)
-// console.log("seriesLength " + seriesLength)
-// console.log("gameNumber " + gameNumber)
-// console.log("leftTeamName " + leftTeamName)
-// console.log("rightTeamName " + rightTeamName)
-// console.log("lTeamSScore " + lTeamSScore)
-// console.log("rTeamSScore " + rTeamSScore)
+
+$(() => {
+    WsSubscribers.init(49322, true, ['game:update_state', 'game:match_created']);
+
+    WsSubscribers.subscribe("game", "update_state", (d) => {
+
+        let scoreLeft = (d['game']['teams'][0]['score']);
+        let scoreRight = (d['game']['teams'][1]['score']);
+
+        //Time updater
+        let time = (d['game']['isOT'] ? '+' : '') + secondsToTime(
+            d['game']['time_seconds'], d['game']['time_milliseconds'],
+            d['game']['time_seconds'] < 60 && !d['game']['isOT'] && d['game']['time'] != 59
+        );
+
+        let teamOne = findPlayers(d['players'], 0);
+        let teamTwo = findPlayers(d['players'], 1);
+
+        //Player names
+        // for (let i = 0; i < teamOne.length; i++) {
+        //     let id = i+1;
+        //     $('#player-name' + id).text(teamOne[i]);
+        //     $('#bubble-number' + id).css("background-color", "rgb(3, 127, 252)");
+        // }
+        // for (let i = 0; i < teamTwo.length; i++) {
+        //     let id = i+5;
+        //     $('#player-name' + id).text(teamTwo[i]);
+        //     $('#bubble-number' + id).css("background-color", "orange");
+        // }
+
+        //Focused Player
+        let target = d['game']['target'];
+        try {
+            target = (d['players'][target]['name']);
+        } catch (e) {
+            // console.log('No player is focused.')
+        }
+
+        ws.send(JSON.stringify({
+            "event": "scorebug-event",
+            "time": time,
+            "Score Left": scoreLeft,
+            "Score Right": scoreRight,
+            "Team One Roster": teamOne,
+            "Team Two Roster": teamTwo,
+            "Target": target,
+        }))
+
+        let teamLeft, teamRight;
+        // Team Names
+        if (teamOne[1] == null) {
+            teamLeft = (teamOne[0]);
+        } else {
+            teamLeft = (d['game']['teams'][0]['name']);
+        }
+        if (teamTwo[1] == null) {
+            teamRight = (teamTwo[0]);
+        } else {
+            teamRight = (d['game']['teams'][1]['name']);
+        }
+
+        ws.send(JSON.stringify({
+            "event": "team-names",
+            "Team Left": teamLeft,
+            "Team Right": teamRight,
+            "Team Left Override": leftTeamName,
+            "Team Right Override": rightTeamName
+        }))
+    })
+
+    WsSubscribers.subscribe("game", "match_created", (d) => {
+
+    })
+})
+
+export { valueHandler };
+
+// Make valueHandler.js handle ALL events.
